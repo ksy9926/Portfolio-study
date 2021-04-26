be57094ac45c71bce640f5d11ba2d9aa3ce4d0a7
수영 김 <ksy9926@naver.com>
2021-04-27 02:34:47 +0900

    Add: project images

--- src/components/Project.js
+++ src/components/Project.js
@@ -21,9 +21,9 @@ function Project() {
           </button>
         </div>
         <div className="work__projects">
-          <a href="" className="project" target="blank" data-type="front-end">
+          <a href="https://github.com/ksy9926/MASL/tree/master" className="project" target="blank" data-type="front-end">
             <img
-              src=""
+              src="./images/masl_result.png"
               alt="MASL"
               className="project__img"
             />
@@ -33,14 +33,14 @@ function Project() {
             </div>
           </a>
           <a
-            href=""
+            href="https://github.com/ksy9926/Webproject-Racer-Library"
             className="project"
             data-type="front-end"
             target="blank"
           >
             <img
               className="project__img"
-              src=""
+              src="./images/library_main.png"
               alt="레이서도서관"
             />
             <div className="project__description">
@@ -49,24 +49,24 @@ function Project() {
             </div>
           </a>
           <a
-            href=""
+            href="https://github.com/ksy9926/Todaktodak/tree/master"
             className="project"
             data-type="front-end"
             target="blank"
           >
-            <img className="project__img" src="" alt="토닥토닥" />
+            <img className="project__img" src="./images/todak_main.png" alt="토닥토닥" />
             <div className="project__description">
               <h3>토닥토닥</h3>
               <span>medical black section solving commutny</span>
             </div>
           </a>
           <a
-            href=""
+            href="https://github.com/ksy9926/Portfolio-study"
             className="project"
             data-type="front-end"
             target="blank"
           >
-            <img className="project__img" src="" alt="포트폴리오" />
+            <img className="project__img" src="./images/portfolio_index.png" alt="포트폴리오" />
             <div className="project__description">
               <h3>포트폴리오</h3>
               <span>Let me introduce myself</span>
