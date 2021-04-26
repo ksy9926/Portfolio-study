0471b1a08aa1904a3294ebb696a3cc1e255a936e
수영 김 <ksy9926@naver.com>
2021-04-27 00:08:00 +0900

    CSS: contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,17 +3,17 @@ import React from 'react';
 function Contact() {
   return (
     <section id="contact" className="section">
-      <h1>Let's talk</h1>
-      <h2>ksy2799nw@gmail.com</h2>
-      <div>
-        <a>
-          <i></i>
+      <h1 className="contact__title">Let's talk</h1>
+      <h2 className="contact__email">ksy2799nw@gmail.com</h2>
+      <div className="contact__links">
+        <a href="https://github.com/ksy9926" target="_blank">
+          <i className="fab fa-github"></i>
         </a>
-        <a>
-          <i></i>
+        <a href="#" target="_blank">
+          <i className="fa fa-linkedin-square"></i>
         </a>
       </div>
-      <p>
+      <p className="contact__rights">
         2021 KSY - All rights reserved
       </p>
     </section>
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,52 +2,77 @@ import React from 'react';
 
 function Project() {
   return (
-    <section>
-      <h1>My work</h1>
-      <h3>Projects</h3>
-      <div>
-        <button>
-          All <span>5</span>
-        </button>
-        <button>
-          Front-end <span>5</span>
-        </button>
-        <button>
-          Back-end <span>2</span>
-        </button>
-        <button>
-          Full-stack <span>1</span>
-        </button>
-      </div>
-      <div>
-        <a>
-          <img />
-          <div>
-            <h3>MASL</h3>
-            <span>Find your 슬세권</span>
-          </div>
-        </a>
-        <a>
-          <img />
-          <div>
-            <h3>레이서도서관</h3>
-            <span>Book you want to study</span>
-          </div>
-        </a>
-        <a>
-          <img />
-          <div>
-            <h3>토닥토닥</h3>
-            <span>medical black section solving commutny</span>
-          </div>
-        </a>
-        <a>
-          <img />
-          <div>
-            <h3>포트폴리오</h3>
-            <span>Let me introduce myself</span>
-          </div>
-        </a>
+    <section id="work" className="section">
+      <div className="section__container">
+        <h1>My work</h1>
+        <h3>Projects</h3>
+        <div className="work__categories">
+          <button className="category__btn selected" data-filter="*">
+            All <span className="category__count">5</span>
+          </button>
+          <button className="category__btn" data-filter="front-end">
+            Front-end <span className="category__count">5</span>
+          </button>
+          <button className="category__btn" data-filter="back-end">
+            Back-end <span>2</span>
+          </button>
+          <button className="category__btn" data-filter="full-stack">
+            Full-stack <span className="category__count">1</span>
+          </button>
+        </div>
+        <div className="work__projects">
+          <a href="" className="project" target="blank" data-type="front-end">
+            <img
+              src=""
+              alt="MASL"
+              className="project__img"
+            />
+            <div className="project__description">
+              <h3>MASL</h3>
+              <span>Find your 슬세권</span>
+            </div>
+          </a>
+          <a
+            href=""
+            className="project"
+            data-type="front-end"
+            target="blank"
+          >
+            <img
+              className="project__img"
+              src=""
+              alt="레이서도서관"
+            />
+            <div className="project__description">
+              <h3>레이서도서관</h3>
+              <span>Book you want to study</span>
+            </div>
+          </a>
+          <a
+            href=""
+            className="project"
+            data-type="front-end"
+            target="blank"
+          >
+            <img className="project__img" src="" alt="토닥토닥" />
+            <div className="project__description">
+              <h3>토닥토닥</h3>
+              <span>medical black section solving commutny</span>
+            </div>
+          </a>
+          <a
+            href=""
+            className="project"
+            data-type="front-end"
+            target="blank"
+          >
+            <img className="project__img" src="" alt="포트폴리오" />
+            <div className="project__description">
+              <h3>포트폴리오</h3>
+              <span>Let me introduce myself</span>
+            </div>
+          </a>
+        </div>
       </div>
     </section>
   );
